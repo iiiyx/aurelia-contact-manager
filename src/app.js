b7e7d7da74d8f574e9ce2a88645f559f0d685d33
Oleg Sobolev <Oleg_Sobolev@epam.com>
2017-12-05 20:04:48 +0300

    init with enhanced contact manager app

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import {inject} from 'aurelia-framework';
+import {WebAPI} from 'api/web-api';
+import { EventAggregator } from 'aurelia-event-aggregator';
+import { ContactCreated } from './messages';
+
+@inject(WebAPI, EventAggregator)
+export class App {
+  constructor(api, ea) {
+    this.api = api;
+    this.ea = ea;
+
+    ea.subscribe(ContactCreated, msg => {
+      this.router.navigate(this.router.generate('contacts', {id: msg.contact.id}));
+    });
+  }
+
+  configureRouter(config, router) {
+    config.title = 'Contacts';
+    // config.options.pushState = true;
+    // config.options.root = '/';
+    config.map([
+      { route: '',              moduleId: 'no-selection',   title: 'Select' },
+      { route: 'contacts/:id',  moduleId: 'contact-detail', name: 'contacts' },
+      { route: 'new',           moduleId: 'contact-detail', name: 'new' }
+    ]);
+
+    this.router = router;
+  }
+}
---
+++ src/contact-detail.js
@@ -0,0 +1,61 @@
+import { inject } from 'aurelia-framework';
+import { EventAggregator } from 'aurelia-event-aggregator';
+import { WebAPI } from 'api/web-api';
+import { ContactUpdated, ContactViewed, ContactCreated } from './messages';
+import { areEqual } from 'utils/utility';
+
+@inject(WebAPI, EventAggregator)
+export class ContactDetail {
+  constructor(api, ea) {
+    this.api = api;
+    this.ea = ea;
+  }
+
+  activate(params, routeConfig) {
+    this.routeConfig = routeConfig;
+
+    if (typeof params.id === 'undefined') {
+      this.contact = {};
+      this.ea.publish(new ContactViewed(this.contact));
+      return;
+    }
+
+    return this.api.getContactDetails(params.id).then(contact => {
+      this.contact = contact;
+      this.routeConfig.navModel.setTitle(contact.firstName);
+      this.originalContact = JSON.parse(JSON.stringify(contact));
+      this.ea.publish(new ContactViewed(this.contact));
+    });
+  }
+
+  get canSave() {
+    return this.contact.firstName && this.contact.lastName && !this.api.isRequesting;
+  }
+
+  save() {
+    this.api.saveContact(this.contact).then(contact => {
+      const id = this.contact.id;
+      this.contact = contact;
+      this.routeConfig.navModel.setTitle(contact.firstName);
+      this.originalContact = JSON.parse(JSON.stringify(contact));
+      this.ea.publish(new ContactUpdated(this.contact));
+      if (typeof id === 'undefined') {
+        this.ea.publish(new ContactCreated(this.contact));
+      }
+    });
+  }
+
+  canDeactivate() {
+    if (!areEqual(this.originalContact, this.contact)) {
+      let result = confirm('You have unsaved changes. Are you sure you wish to leave?');
+
+      if (!result) {
+        this.ea.publish(new ContactViewed(this.contact));
+      }
+
+      return result;
+    }
+
+    return true;
+  }
+}
